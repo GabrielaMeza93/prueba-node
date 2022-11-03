932db93ba5a42de465cbc3a9fa08cbe810ad0e85
gabrielameza93 <gabrielameza1993@gmail.com>
2022-11-03 10:59:54 -0300

    mi primer commit

---
+++ callback.js
@@ -0,0 +1,4 @@
+let destinosDelMundo = ['Marruecos', 'Bariloche', 'Barcelona', 'China', 'Grecia'];
+let [marruecos, bariloche, barcelona, china, grecia] = destinosDelMundo;
+console.log(bariloche);
+console.log(china)
